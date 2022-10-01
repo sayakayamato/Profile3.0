cdbb1ea797f9b44a5fa027734dddb69862c46838
rahydyn <kanaokasatoru@gmail.com>
2022-10-02 08:09:47 +0900

    modified from "unregister" to "register"

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 // serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
